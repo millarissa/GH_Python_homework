a4f7ab6a0b4deaa27e77915633b945d87e805cb9
millarissa <millarissa.valdes@gmail.com>
2022-10-14 13:20:02 +0200

    HW_3 task_6 fixed

--- HW_3/task_6.py
+++ HW_3/task_6.py
@@ -3,11 +3,20 @@ Write a script to get the maximum and minimum VALUE
 in a dictionary.
 """
 
-dict_1 = {1: 25, 2: 58, 3: 33341, 4: 12}
+dict_1 = {1: 43, 2: 85, 3: 'asdfg', 4: 3411, 5: 1}
 
-dict_values = dict_1.values()
+dict_2 = {}
+for key,value in dict_1.items():
+    if type(value) == int:
+        dict_2[key] = value
+
+dict_values = dict_2.values()
+        
 max_val = max(dict_values)
 min_val = min(dict_values)
 
 print('Maximum value: ', max_val)
 print('Minimum value: ', min_val)
+
+
+
