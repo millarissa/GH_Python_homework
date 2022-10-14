07739c584ac6552ad7f6ca372e6113ae86e347c4
millarissa <millarissa.valdes@gmail.com>
2022-10-14 03:17:38 +0200

    HW_1 task_1 fixed

--- HW_3/task_1.py
+++ HW_3/task_1.py
@@ -6,12 +6,13 @@ The "replacement" value is entered by user.
 The number of elements in the tuples must be different.
 """
 
-list_tuples = [(1,2,3), ('S','P','Q','R'), ('a','b'), (8, 7, 6, 5, 4)]
+list_tuples = [(1,2,3), ('S','P','Q','R'), ('a','b'), (), (8, 7, 6, 5, 4)]
 change_val = input('Enter replacement value: ')
 
 for i,tuples in enumerate(list_tuples):
     tup_list = list(list_tuples[i])
-    tup_list[-1] = change_val
+    if list_tuples[i]:
+        tup_list[-1] = change_val
     list_tuples[i] = tuple(tup_list)
     
 print(list_tuples)
