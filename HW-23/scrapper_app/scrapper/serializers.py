91522da6358c62043dbedd3beae5e1c003733efb
millarissa <millarissa.valdes@gmail.com>
2023-02-27 20:34:11 +0200

    Homework 23

---
+++ HW-23/scrapper_app/scrapper/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from .models import Category, Product, IdStorage
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'category_title'
+        )
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'item_id',
+            'title',
+            'href',
+            'current_price',
+            'old_price',
+            'brand',
+            'category',
+            'sell_status'
+        )
+
+
+class IdStorageSerializer(serializers.Serializer):
+    product_ids = serializers.CharField()
+
+
